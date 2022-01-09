5af7ddc842d32ba6ef69131262fc40676fa9f54c
munkatchy <96927690+munkatchy@users.noreply.github.com>
2022-01-09 22:30:33 +0000

    Created pre02 folder

---
+++ pre02/Date.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Date{
+  public static void main(String[] args){
+    System.out.println("Hello, World!");
+    System.out.println("What is today's date?");
+
+
+  }
+
+
+}
