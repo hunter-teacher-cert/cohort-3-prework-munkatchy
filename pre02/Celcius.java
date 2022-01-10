3218197c74848200a566ce405ef1917cd5b8b6f6
munkatchy <96927690+munkatchy@users.noreply.github.com>
2022-01-10 00:16:18 +0000

    Just practicing chapter 3 sections before the exercises

---
+++ pre02/Celcius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Celcius{
+
+  public static void main(String[] args) {
+    String line;
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Type something: ");
+    line = in.nextLine();
+    System.out.println("You said: " + line);
+
+
+  }
+}
