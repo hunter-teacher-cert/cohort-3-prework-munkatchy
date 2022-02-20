9daa706bd3b4c9e8fdcb928787b16bc55342ae31
munkatchy <96927690+munkatchy@users.noreply.github.com>
2022-02-20 23:31:12 +0000

    Finished Chap 3 exercise 2 called Celcius

--- pre02/Celcius.java
+++ pre02/Celcius.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class Celcius{
 
   public static void main(String[] args) {
-    String line;
+    //define the variables and constants
+    double tempinC;
+    double tempinF;
+    final double C_TO_F_CONVERSION = 1.8;
     Scanner in = new Scanner(System.in);
 
-    System.out.print("Type something: ");
-    line = in.nextLine();
-    System.out.println("You said: " + line);
+    //prompt the user for a Celcius temp with one decimal place
+    System.out.print("What is the temp today in Celcius? ");
+    tempinC = in.nextDouble();
+
+    //convert and output
+    tempinF = (tempinC * C_TO_F_CONVERSION) + 32;
+    System.out.printf("%.1f C = %.1f F\n", tempinC, tempinF);
 
 
   }
